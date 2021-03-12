3cdc571ff320aafa097e6188a46688bd8dce07ee
张坤徉 <https://github.com/chichuhanye/groupGame>
2021-03-12 14:54:48 +0800

    开始界面张坤徉 v0.1

--- src/MainGame.java
+++ src/MainGame.java
@@ -16,7 +16,7 @@ import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 
-// 改改
+// zky部分
 
 public class MainGame implements ApplicationListener {
 
